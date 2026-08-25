658876cb22d6d5336f067771775dc2766f1d0756
sir1003 <126301825+sir1003@users.noreply.github.com>
2026-08-25 13:08:11 +0700

    Delete Classes directory

--- Classes/KioskViewController.h
+++
@@ -1,8 +0,0 @@
-#import <UIKit/UIKit.h>
-
-@interface KioskViewController : UIViewController <UIWebViewDelegate>
-
-@property (nonatomic, strong) UIWebView *webView;
-@property (nonatomic, strong) UIActivityIndicatorView *spinner;
-
-@end
